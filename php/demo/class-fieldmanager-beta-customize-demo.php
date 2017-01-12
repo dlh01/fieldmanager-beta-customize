36f5653db246227d0bc110ed879c4b05c7532d62
David Herrera <david@alleyinteractive.com>
2017-01-11 23:16:52 -0500

    Add demo of repeatable RichTextArea

--- php/demo/class-fieldmanager-beta-customize-demo.php
+++ php/demo/class-fieldmanager-beta-customize-demo.php
@@ -133,6 +133,16 @@ class Fieldmanager_Beta_Customize_Demo {
 			'sanitize' => 'intval',
 		) );
 		fm_beta_customize_add_to_customizer( 'Fieldmanager Validated Fields', $fm );
+
+		$fm = new Fieldmanager_Group( array(
+			'name' => 'repeatable_richtextarea',
+			'limit' => 0,
+			'one_label_per_item' => false,
+			'children' => array(
+				'richtext' => new Fieldmanager_RichTextArea(),
+			),
+		) );
+		fm_beta_customize_add_to_customizer( 'Fieldmanager Repeatable Fields', $fm );
 	}
 
 	/**
